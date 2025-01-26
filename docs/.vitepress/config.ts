312305de81fafa68c11ef57861b48a98610db25b
Chris <chrisbreuer93@gmail.com>
2025-01-26 15:11:11 +0100

    chore: update og image

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -90,7 +90,7 @@ export default withPwa(
       ['meta', { property: 'og:description', content: description }],
 
       ['meta', { property: 'og:site_name', content: 'dnsx' }],
-      ['meta', { property: 'og:image', content: './images/og-image.jpg' }],
+      ['meta', { property: 'og:image', content: './images/og-image.png' }],
       ['meta', { property: 'og:url', content: 'https://dnsx.netlify.app/' }],
       // ['script', { 'src': 'https://cdn.usefathom.com/script.js', 'data-site': '', 'data-spa': 'auto', 'defer': '' }],
       ...analyticsHead,
